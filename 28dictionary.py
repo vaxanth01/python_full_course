5c11c6af6f97d08ac9c4840585448303c6cd8d95
vaxanth01 <vaxanth01@gmail.com>
2024-07-22 09:33:11 +0530

    updates

---
+++ 28dictionary.py
@@ -0,0 +1,55 @@
+user={
+    "name":"vasanth",
+    "age":21,
+    "district":"salem",
+    "state":"tamilnadu",
+}
+
+# print(user)
+# print(type(user))
+# print(user["age"])
+# print(user.keys())
+# print(user.values())
+# print(user.items())
+# print(user.pop("district"))
+# print(user)
+# print("_____________________")
+# for name in user:
+#     # print(name)
+#     # print(user[name])
+#     print(name,": ",user[name])
+
+
+# for name in user.items():
+    # print(name)   
+
+# if "age" in user:
+#     print("yes")
+# else:
+#     print("no")    
+# for x in user:
+#     print(x,":",user[x])
+# user.update({"degree":"B tech"})
+# print(user)
+# user["age"]=22
+# print(user["age"])
+# del user
+# print(user)
+print("______________nested_dictionary__________________")
+
+collage={
+    "arts":{"department":"M.com",
+            "fess":35000,
+            "block":"A-block"},
+
+    "engineering":{"department":"CSE",
+            "fess":935000,
+            "block":"A-block"},
+
+    "medical":{"department":"B pahrm",
+            "fess":43500,
+            "block":"A-block"}
+}
+
+# print(collage)
+# print(collage["arts"])
